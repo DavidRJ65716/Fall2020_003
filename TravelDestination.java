1d0b57cfe84640d517f24fc58401206c7b6653e6
David Ryan Jackson <daverj65716@gmail.com>
2020-10-06 14:24:37 -0500

    Create TravelDestination.java
    created .java file

---
+++ TravelDestination.java
@@ -0,0 +1,8 @@
+public class TravelDestination {
+
+	public static void main(String[] args) {
+	
+    //Adding a print statement with name and hobby
+		System.out.println("My name is David Jackosn. One of my hobbies is Fishing.");
+		}
+}
